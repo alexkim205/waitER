5f558d6d9b58f92c8fd5826234df4fb5e2c63251
Alex Gyujin Kim <agk2144@columbia.edu>
2018-09-08 12:57:48 -0400

    separate data

--- data/manipulatedata.R
+++ data/manipulatedata.R
@@ -2,4 +2,7 @@ library(data.table)
 d <- fread("AdmissionsCorePopulatedTable.txt", sep="\t", header=TRUE)
 ESI <- sample.int(5, 100, replace=TRUE)
 d <- cbind(d[!duplicated(d$PatientID)], "ESI"=ESI)
-write.csv(d, "ESI.data.csv", row.names = FALSE)
+d_inER <- d[1:20,]
+d_reserve <- d[21:80,]
+write.csv(d_inER, "ESI.inER.csv", row.names = FALSE)
+write.csv(d_reserve, "ESI.reserve.csv", row.names = FALSE)
