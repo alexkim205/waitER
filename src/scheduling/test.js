c9ba42778c3eb08dba3fc5d6615ce65b98de435f
eutopi <tongyuzhou98@gmail.com>
2018-09-08 15:55:55 -0400

    reorganized file locations

---
+++ src/scheduling/test.js
@@ -0,0 +1,26 @@
+var fs = require("fs");
+var express = require("express");
+var app = express();
+
+
+function runtest(callback){
+    app.listen(3000, function() {
+	    console.log("server running, port 3000");
+	} )
+
+	var spawn = require("child_process").spawn;
+    var process = spawn("python",["schedule.py"]);
+    process.stdout.on("data",function(data){
+	    var text = data.toString("utf8");// buffer to string
+	    callback(text);
+	});
+}
+
+runtest(function(data){
+	console.log(text);
+    });
+
+/*
+fs.readFile("/Users/Tongyu/hackathon/PennApps/data/ESI.data.csv", "utf8", function(err, contents){
+    makelist(contents)
+    });*/
