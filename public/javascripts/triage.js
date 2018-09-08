be16f75ed7127c0615425e7fb6b09e1cabd06aa4
Alex Gyujin Kim <agk2144@columbia.edu>
2018-09-08 17:53:44 -0400

    triage stuff formatting html

--- public/javascripts/triage.js
+++ public/javascripts/triage.js
@@ -1,10 +1,14 @@
 // require('parsleyjs')
 // require('pretty-checkbox')
+console.log('Triage.js loaded')
 
-$('.option').on('click', function() {
-    $(this).removeClass('selected')
-    $(this).addClass('selected')
-    $(this).css({
-        'font-size': '1.3em'
+$(document).ready(function() {
+    $('.option')
+    .click(function() {
+        console.log('clicked')
+        let options = $(this).siblings().andSelf()
+        options.removeClass('selected')
+        $(this).addClass('selected')
     })
-})
+});
+
