6330d449cd72b36e750d278194e3c60736a6c803
Alex Gyujin Kim <agk2144@columbia.edu>
2018-09-09 02:37:10 -0400

    wired up everything to new patient.pug

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,12 +6,6 @@ var headers = {
     'Authorization': 'Bearer ' + 'demo',
     'Accept': 'application/json'
 };
-var url = 'https://api.humanapi.co/v1/human/activities/summaries?access_token=demo&source=myfitnesspal&limit=10'
-
-getdata.getUserData(url, headers, function (error, data) {
-    console.log(error);
-    console.log(data);
-});
 
 function requestHelp(){
     network.sendData("help-request");
--- routes/patient.js
+++ routes/patient.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/:clientId', function(req, res, next) {
-  res.render('patientindex', { title: 'PennApps', id: req.params['clientId'] });
+  res.render('patient', { title: 'WaitER', id: req.params['clientId'] });
 });
 
 module.exports = router;
