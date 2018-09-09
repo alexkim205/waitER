06f6629bce7ce9992f0ffb176a6fced58d25dc0e
eutopi <tongyuzhou98@gmail.com>
2018-09-08 22:48:25 -0400

    add and remove operators added

--- src/scheduling/schedule.py
+++ src/scheduling/schedule.py
@@ -1,13 +1,12 @@
 from collections import namedtuple
 from operator import attrgetter
 import bisect
-import random
 import sys
 '''                                                                                                                                                            
 Interval scheduling for patients weighted on ESI                                                                                                               
 '''
 
-Patient = namedtuple('Patient', 'ID start_time end_time duration ESI')
+Patient = namedtuple('Patient', 'ID start_time end_time ESI')
 
 # stores file in dictionary of named tuples                                                                                                                    
 def make_list(file="/Users/Tongyu/hackathon/PennApps/data/ESI.inER.csv"):
@@ -18,19 +17,24 @@ def make_list(file="/Users/Tongyu/hackathon/PennApps/data/ESI.inER.csv"):
     content.pop(0)
     for line in content:
         l = line.split(',')
-        list.append(Patient(l[0][1:-1], l[2][1:-1], l[3][1:-1], random.randint(1,101), l[4]))
+        list.append(Patient(l[0][1:-1], l[2][1:-1], l[3][1:-1], l[4]))
     patients = sorted(list, key=attrgetter('ESI'))
-#    print(patients, flush=True)
+    print(patients, flush=True)
     return patients
 
-def add(patient):
-    list = make_list()
+def add(patient, list):
     l = patient.split(',')
-    new = Patient(l[0][1:-1], l[2][1:-1], l[3][1:-1], random.randint(1,101), l[4])
+    new = Patient(l[0][1:-1], l[2][1:-1], l[3][1:-1], l[4])
     list.append(new)
     patients = sorted(list, key=attrgetter('ESI'))
     index = patients.index(new)
     print((index, patients), flush=True)
+
+def remove(patient, list):
+    l = patient.split(',')
+    old = Patient(l[0][1:-1], l[2][1:-1], l[3][1:-1], l[4])
+    list.remove(old)
+    print(list, flush=True)
 
 if __name__ == '__main__':
 #    filename = "/Users/Tongyu/hackathon/PennApps/data/ESI.data.csv"                                                                                    
@@ -38,4 +42,5 @@ if __name__ == '__main__':
         content = f.readlines()
         content = [x.strip() for x in content]
      first = content[1]
-     add(first)
+     list = make_list()
+     add(first, list)
--- src/scheduling/test.js
+++ src/scheduling/test.js
@@ -6,27 +6,50 @@ var readline = require("readline");
 var myInterface = readline.createInterface({
   input: fs.createReadStream("/Users/Tongyu/hackathon/PennApps/data/ESI.reserve.csv")
 });
+var spawn = require("child_process").spawn;
+var patientno = 0;
+var patientList;
 
-var lineno = 0;
-myInterface.on('line', function (line) {
-  lineno++;
-  runtest(line, (function(data){
-    console.log(data);
-  });
-});
-
-function runtest(line, callback){
-  app.listen(3000, function() {
-    console.log("server running, port 3000");
-  } )
-  var spawn = require("child_process").spawn;
-  var process = spawn("python",["-c", `import schedule; print (schedule.add(${line}))`]);
+function makeList(callback){
+  var process = spawn("python",["-c", `import schedule; print (schedule.make_list()`]);
   process.stdout.on("data",function(data){
     var text = data.toString("utf8");// buffer to string
     callback(text);
   });
 }
 
+// operation field is either "add" or "remove", else returns error
+function update(operation, patient, list, callback){
+  if(operation ===`add` || operation ===`remove`){
+    var process = spawn("python",["-c", `import schedule; print (schedule.${operation}(${patient}, ${list}))`]);
+    process.stdout.on("data",function(data){
+      var text = data.toString("utf8");// buffer to string
+      callback(false, text);
+    });
+  }
+  else {
+    callback(true, null);
+  }
+}
+
+/// calls
+makeList((function(data){
+  patientList = data;
+  console.log(data);
+}));
+
+myInterface.on('line', function (line) {
+  patientno++;
+  patient = line
+  update(`add`, patient, patientList, function(err, data){
+    if(err){
+      throw new Error('Invalid operation');
+    }
+    else {
+      console.log(data);
+    }
+  });
+});
 
   /*
 fs.readFile("/Users/Tongyu/hackathon/PennApps/data/ESI.data.csv", "utf8", function(err, contents){
